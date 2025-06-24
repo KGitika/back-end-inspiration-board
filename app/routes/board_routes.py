046e10e467d74b2e293f129d08745a6732e9c4f2
Camille <camilledubois1995@gmail.com>
2025-06-24 14:57:36 -0400

    implemented routes utilities

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -5,3 +5,18 @@ from app.routes.routes_utilities import validate_model, create_model
 from ..db import db
 
 bp = Blueprint("boards_bp", __name__, url_prefix="/boards")
+
+@bp.post("")
+def create_board():
+    """
+    Create a new board.
+    """
+    data = request.get_json()
+    if not data:
+        abort(400, "No data provided")
+
+    board = create_model(Board, data)
+    db.session.add(board)
+    db.session.commit()
+
+    return make_response(board.to_dict(), 201)
