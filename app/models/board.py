4ea8d389544a59b9c4adedad6ec40c8a03a4e2af
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-06-25 00:58:52 -0700

    implemented board model with to_dict() and from_dict()

---
+++ app/models/board.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from ..db import db
+# from sqlalchemy import ForeignKey
+from typing import List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .card import Card
+
+
+class Board(db.Model):
+    __tablename__ = "board"  # Table name in the database
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    owner: Mapped[str] = mapped_column(nullable=False)
+
+    cards: Mapped[List["Card"]] = relationship(back_populates="board")
+
+    def to_dict(self):
+        board_as_dict = {
+            "id": self.id,
+            "title": self.title,
+            "owner": self.owner
+        }
+        if self.cards:
+            board_as_dict["cards"] = [card.to_dict() for card in self.cards]
+        else:
+            board_as_dict["cards"] = []
+        return board_as_dict
+
+    @classmethod
+    def from_dict(cls, board_data):
+        # return Board(title=board_data["title"], owner=board_data["owner"])
+        return cls(title=board_data["title"], owner=board_data["owner"])
---
+++ app/models/card.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from ..db import db
+from typing import Optional
+
+
+class Card(db.Model):
+
+    # The Card model represents a card that belongs to a specific board.
+    # Each card contains a message and a count of likes.
+
+    __tablename__ = "cards"  # Table name in the database
+
+    # Primary key for each card, auto-incremented
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    # The content/message of the card (cannot be null)
+    message: Mapped[str] = mapped_column(nullable=False)
+    # Number of likes for the card (default is 0)
+    likes_count: Mapped[int] = mapped_column(default=0)
+    # Foreign key referencing the 'boards' table (must be linked to a board)
+    board_id: Mapped[int] = mapped_column(
+        ForeignKey("boards.id"), nullable=False)
+
+    # Relationship to the Board model (each card is linked to one board)
+    board: Mapped["Board"] = relationship("Board", back_populates="cards")
+
+# Helper method to convert the card object into a dictionary for JSON responses.
+
+
+def to_dict(self):
+    card_dict = {
+        "card_id": self.id,
+        "message": self.message,
+        "likes_count": self.likes_count,
+        "board_id": self.board_id
+    }
